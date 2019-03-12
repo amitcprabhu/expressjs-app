503f46008a7410cfd6e25c77a57120a8d8d63455
unknown <amitcprabhu@gmail.com>
2019-03-12 22:33:40 +0530

    added json dat

--- routes/users.js
+++ routes/users.js
@@ -3,19 +3,22 @@ var bodyParser = require('body-parser')
 
 var router = express.Router();
 
-const users=[{id:1,name:'amit',city:'pune'},{id:2,name:'pradnya',city:'pune'}];
+const users = [{ id: 1, name: 'amit', city: 'pune' }, 
+{ id: 2, name: 'pradnya', city: 'pune' }, 
+{ id: 2, name: 'pradnya', city: 'pune' }, 
+{ id: 2, name: 'pradnya', city: 'pune' }];
 
 // create application/json parser
 var jsonParser = bodyParser.json()
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  const result = req.query.id!=undefined ? users.filter(val => val.id == req.query.id):users;
+router.get('/', function (req, res, next) {
+  const result = req.query.id != undefined ? users.filter(val => val.id == req.query.id) : users;
   res.json(result);
 });
 
 /* Post users listing. */
-router.post('/', jsonParser,function(req, res, next) {
+router.post('/', jsonParser, function (req, res, next) {
   users.push(req.body);
   console.log(users);
   res.json(users)
